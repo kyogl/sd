d0c722dc47e33bb124ac5ae89f58f53921fc8d91
kyogl <nsmaomao5@gmail.com>
2023-03-02 20:04:02 +0800

    test

--- webui.py
+++ webui.py
@@ -185,12 +185,8 @@ def api_only():
     app.add_middleware(GZipMiddleware, minimum_size=1000)
     api = create_api(app)
 
-    modules.script_callbacks.before_ui_callback()
-
     modules.script_callbacks.app_started_callback(None, app)
-
-    extra_networks.initialize()
-    extra_networks.register_extra_network(extra_networks_hypernet.ExtraNetworkHypernet())
+    modules.script_callbacks.before_ui_callback()
 
     api.launch(server_name="0.0.0.0" if cmd_opts.listen else "127.0.0.1", port=cmd_opts.port if cmd_opts.port else 7861)
 
